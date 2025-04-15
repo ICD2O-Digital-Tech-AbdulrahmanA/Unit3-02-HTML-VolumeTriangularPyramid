9dbd33e92054c7d1432060833303297e9821f914
Abdulrahman Albinali <abdulrahman.albinali@stu.ocsb.ca>
2025-04-15 00:51:45 +0000

    Volume of a Right Angle Pyramid

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Ms Raffin All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Ms Raffin
+// Created on: March 2022
 // This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+
+
+function calculate () {
+  // turn user input into variables
+    let heightP = parseFloat(document.getElementById('height-of-pyramid').value);
+    let base = parseFloat(document.getElementById('base-of-triangle').value);
+    let heightT = parseFloat(document.getElementById('height-of-triangle').value);
+
+  // process
+  let volume = (1/6) * heightP * base * heightT
+  // display final information
+  document.getElementById('Volume').innerHTML = 'Volume is: ' + volume + ' cm^3'
+}
